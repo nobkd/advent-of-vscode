2c527e3c511f41361fae7bcc1eef94a3fcb7bedd
nobkd <44443899+nobkd@users.noreply.github.com>
2022-11-16 00:05:49 +0100

    feat: add views

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('advent-of-code-vscode.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('advent-of-code-vscode.startAoC', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from AdventOfCode!');
+		vscode.window.showInformationMessage('Hello World from Advent of Code!');
 	});
 
 	context.subscriptions.push(disposable);
