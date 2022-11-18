b696196621cd52193376192366f25fff4e545657
nobkd <44443899+nobkd@users.noreply.github.com>
2022-11-19 00:40:26 +0100

    feat: add calculator for every december day from 2015 till now

--- src/view/selectDayView.ts
+++ src/view/selectDayView.ts
@@ -5,10 +5,45 @@ export class SelectDayView {
         const view = vscode.window.createTreeView('selectDayView', { treeDataProvider: provider(), showCollapseAll: true, canSelectMany: false });
         context.subscriptions.push(view);
     }
+}
 
+function generateTree(endYear: number, endMonth: number, endDay: number): Map<number, number[]> {
+    const currentMonthIsDecember = endMonth === 12;
 
+    const years: number[] = [];
+    for (let yearCounter = 2015; yearCounter <= (currentMonthIsDecember ? endYear : endYear - 1); yearCounter++) {
+        years.push(yearCounter);
+    }
+
+    const days: number[] = [];
+    for (let dayCounter = 1; dayCounter <= 31; dayCounter++) {
+        days.push(dayCounter);
+    }
+
+    const currentDays: number[] = [];
+    if (currentMonthIsDecember) {
+        for (let dayCounter = 1; dayCounter <= endDay; dayCounter++) {
+            currentDays.push(dayCounter);
+        }
+    }
+
+    const data: Map<number, number[]> = new Map<number, number[]>();
+    years.forEach(element => {
+        data.set(element, days);
+    });
+
+    if (years.indexOf(endYear) !== -1) {
+        data.set(endYear, currentDays);
+    }
+
+    console.log(data);
+
+    return data;
 }
 
 function provider(): vscode.TreeDataProvider<{ key: string }> {
-    return {};
+    const date: Date = new Date();
+    const tree = generateTree(date.getFullYear(), 12 /*date.getMonth()*/, date.getDate());
+
+    return tree;
 }
