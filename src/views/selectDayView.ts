00690d7c47f01ac2605e7fc6034785b05d7cb347
nobkd <44443899+nobkd@users.noreply.github.com>
2022-11-21 20:31:09 +0100

    fix: correct menu item display, fix tree content

--- src/views/selectDayView.ts
+++ src/views/selectDayView.ts
@@ -1,8 +1,10 @@
+import { getProfileArguments } from '@vscode/test-electron/out/runTest';
 import * as vscode from 'vscode';
 
 type Tree = object & {
     key: number,
-    children: Tree[] | null
+    year?: number,
+    children?: Tree[]
 };
 
 // https://code.visualstudio.com/api/extension-guides/tree-view
@@ -32,13 +34,19 @@ function generateTree(): Tree[] {
     counter = 1;
     const fullMonths: number[] = [...new Array(decemberDays)].map(() => counter++);
 
-    const fullMonthsObjects: Tree[] = fullMonths.map((value: number, index: number, array: number[]) => {
-        return { key: value, children: null };
+    const fullMonthsObjects: Tree[] = fullMonths.map((itemValue: number) => {
+        return { key: itemValue };
     });
     const currentMonthObjects: Tree[] = isDecember ? fullMonthsObjects.slice(0, day) : [];
 
-    const yearsObjects: Tree[] = years.map((value: number, index: number, array: number[]) => {
-        return { key: value, children: value !== year ? fullMonthsObjects : currentMonthObjects };
+    const yearsObjects: Tree[] = years.map((itemValue: number) => {
+        let children: Tree[] = itemValue !== year ? fullMonthsObjects : currentMonthObjects;
+        children = children.map((treeItem: Tree) => treeItem = { ...treeItem, year: itemValue });
+
+        return {
+            key: itemValue,
+            children: children
+        };
     });
 
     return yearsObjects;
@@ -46,21 +54,18 @@ function generateTree(): Tree[] {
 
 function provider(): vscode.TreeDataProvider<Tree> {
     const tree: Tree[] = generateTree();
-    console.log(tree);
 
     return {
         getChildren: (element: Tree | undefined): Tree[] | undefined => {
-            if (element === undefined) {
-                return tree;
-            }
-
-            return element.children || undefined;
+            return element === undefined ? tree : element.children;
         },
         getTreeItem: (element: Tree): vscode.TreeItem => {
             return {
                 label: element.key.toString(),
-                collapsibleState: element && element.children?.length ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None
+                collapsibleState: element && element.children?.length ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None,
+                contextValue: element.children === null ? "day" : "year",
+                //command: set `selected` with element.year, element.day
             };
-        },
+        }
     };
 }
