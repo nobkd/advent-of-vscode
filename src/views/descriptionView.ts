b9a85f0b5dcf3bbf76a004a0e1b88674493b4a34
nobkd <44443899+nobkd@users.noreply.github.com>
2022-11-23 11:21:41 +0100

    fix: add csp & viewport sizing

--- src/views/descriptionView.ts
+++ src/views/descriptionView.ts
@@ -25,7 +25,9 @@ export class DescriptionView implements vscode.WebviewViewProvider {
         <!DOCTYPE html>
         <html lang="en">
             <head>
-                <meta charset="UTF-8">
+                <meta charset="UTF-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; script-src ${webviewView.webview.cspSource}; style-src ${webviewView.webview.cspSource};"/>
                 <!-- TODO: add styles -->
             </head>
             <body>
