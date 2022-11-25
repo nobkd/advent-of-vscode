93940746f2d2452a6e7f09c5bf82ca3a34515ee7
nobkd <44443899+nobkd@users.noreply.github.com>
2022-11-25 22:47:27 +0100

    feat: implement logout logic

--- src/commands/logout.ts
+++ src/commands/logout.ts
@@ -1,11 +1,13 @@
 import * as vscode from 'vscode';
 
-export function logout(context: vscode.ExtensionContext, args: Array<any> | undefined = undefined): void {
-
-	// TODO: delete cookie from secrets
-	// TODO: implement logic to check if cookie really is deleted
-
+export async function logout(context: vscode.ExtensionContext, args: Array<any> | undefined = undefined): Promise<void> {
 	context.secrets.delete('advent-of-vscode.loginCookie');
 
-	vscode.commands.executeCommand('setContext', 'advent-of-vscode.loggedIn', false);
+	if (await context.secrets.get('advent-of-vscode.loginCookie') === undefined) {
+		vscode.window.showInformationMessage('Successfully logged out of AoC');
+		vscode.commands.executeCommand('setContext', 'advent-of-vscode.loggedIn', false);
+	}
+	else {
+		vscode.window.showErrorMessage('Failed to log out of AoC.');
+	}
 }
