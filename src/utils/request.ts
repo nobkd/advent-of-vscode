d560ed4dae3841ed8a88fa8911e803f9fd461586
nobkd <44443899+nobkd@users.noreply.github.com>
2022-11-25 19:10:30 +0100

    feat: load full day if logged in, getData can fail

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -10,29 +10,49 @@ export async function getDescription(year: number, day: number): Promise<string>
     const { data, status, statusText } = await axios.get(`/${year}/day/${day}`, {
         baseURL: base,
         responseType: 'document',
+        ...(await getCookieObject()),
     });
 
     if (status === 200) {
         const dom = new JSDOM(data);
-        return dom.window.document.body.getElementsByClassName('day-desc')[0].innerHTML;
+        const dayDesc = dom.window.document.body.getElementsByClassName('day-desc');
+
+        /// loading all parts if logged in & part I completed
+        let html = '';
+        for (let i = 0; i < dayDesc.length; i++) {
+            html += `<details open><summary>Part ${i + 1}</summary>${dayDesc[i].innerHTML}</details>`;
+        }
+        return html;
     }
     return statusText;
 }
 
 
 export async function getData(year: number, day: number): Promise<string> {
-    const cookie: string = await vscode.commands.executeCommand('advent-of-vscode.loadCookie');
+    const cookieObject: object = await getCookieObject();
+    if (Object.keys(cookieObject).length < 1) {
+        return 'You are not logged in. [Log in](command:advent-of-vscode.login)';
+    }
+
     const { data, status, statusText } = await axios.get(`/${year}/day/${day}/input`, {
-        headers: { cookie: `session=${cookie};` },
         baseURL: base,
         responseType: 'text',
         transformResponse: [],
+        ...cookieObject,
     });
 
     if (status === 200) {
         return data;
     }
     return statusText;
+}
+
+async function getCookieObject(): Promise<object> {
+    const cookie: string | undefined = await vscode.commands.executeCommand('advent-of-vscode.loadCookie');
+    if (cookie !== undefined) {
+        return { headers: { cookie: `session=${cookie};` } };
+    }
+    return {};
 }
 
 export async function testCookie(cookie: string | undefined): Promise<boolean> {
